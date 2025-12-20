adf728673505885edf3fc3cf2ac15baf4ed72ebe
Piotr Popiołek <piotr.popiolek93@gmail.com>
2025-12-20 18:16:23 +0100

    Enhance Dockerfile and entrypoint script: Add support for configurable PORT environment variable in Nginx and entrypoint, improving flexibility for deployment. Update Nginx configuration to listen on the specified PORT, and implement a health check script for better service monitoring.

--- astro/docker-entrypoint.sh
+++ astro/docker-entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 set -e
 
-# Default backend URL if not set
+# Default values
 BACKEND_URL=${BACKEND_URL:-http://backend:8000}
+PORT=${PORT:-8080}
 
-# Substitute environment variables in nginx config
-envsubst '${BACKEND_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+# Export PORT for envsubst
+export PORT
+
+# Substitute environment variables in nginx config template
+envsubst '${BACKEND_URL} ${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Start nginx
 exec nginx -g 'daemon off;'
