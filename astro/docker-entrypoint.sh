c7855ad0b3a2788be877c20da331aebf503e8be6
Piotr Popiołek <piotr.popiolek93@gmail.com>
2025-12-21 21:09:53 +0100

    Update Dockerfile and entrypoint script for Nginx PID file handling: Modify the entrypoint script to specify the PID file location for Nginx, and adjust the Dockerfile to ensure the PID file is writable by the non-root user, enhancing compatibility and security in the container environment.

--- astro/docker-entrypoint.sh
+++ astro/docker-entrypoint.sh
@@ -10,5 +10,5 @@ export PORT
 # Substitute environment variables in nginx config template
 envsubst '${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
-# Start nginx
-exec nginx -g 'daemon off;'
+# Start nginx (pid file is configured in nginx.conf to use /tmp/nginx.pid)
+exec nginx -g 'daemon off; pid /tmp/nginx.pid;'
